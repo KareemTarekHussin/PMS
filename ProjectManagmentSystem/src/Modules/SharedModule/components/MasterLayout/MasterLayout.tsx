5c46d3657f71d070bccfa2f9832b6a408669e62e
omarShousha <oshousha6@gmail.com>
2024-05-15 18:57:54 +0300

    Modified changePassword and sidebar code in SideBar.tsx & Finished Navbar.tsx

--- ProjectManagmentSystem/src/Modules/SharedModule/components/MasterLayout/MasterLayout.tsx
+++ ProjectManagmentSystem/src/Modules/SharedModule/components/MasterLayout/MasterLayout.tsx
@@ -6,12 +6,12 @@ import { Outlet } from 'react-router-dom'
 export default function MasterLayout() {
   return (
     <>
+      <Navbar/>
       <div className="d-flex">
         <div>
           <SideBar/>
         </div>
-        <div className='w-100 vh-100 p-2 p-md-3 overflow-y-auto bg-info-subtl'>
-          <Navbar/>
+        <div className='w-100 vh-100 p-2 p-md-3  bg-info-subtle overflow-y-auto bg-info-subtl'>
           <Outlet/>
         </div>
       </div>
